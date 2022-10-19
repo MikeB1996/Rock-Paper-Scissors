01f7e99133be8fa6971f8ec1f8790ca02d47548d
MikeB1996 <mikelbarko2@gmail.com>
2022-10-19 14:19:25 +0200

    html and js changes about the game

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 //generates numbers from 0 to 2 and assigns them to the 3 choices for Rock/Paper/Scissors
+const winningPoints= 5;
+
 
 function getComputerChoice() {
     let num= Math.floor(Math.random() * 3);
@@ -35,37 +37,57 @@ function buttonClick3() {
 
 
 //function that gets in input player choice and computer choice and decides who wins
+// it returns winValue that show who won this round (0=draw,1= the computer won,2= the player won)
 function RockPaperScissors(playerChoice,computerChoice) {
-  
+    let winValue
   switch (true){
     case (playerChoice==="Rock" || computerChoice==="Rock"):
         alert("It's a draw");
+        winValue= 0;
+        return winValue;
         break;
     case (playerChoice==="Rock" || computerChoice==="Paper"):
         alert("The computer wins");
+        winValue= 1;
+        return winValue;
         break;
     case (playerChoice==="Rock" || computerChoice==="Scissors"):
             alert("You win");
+            winValue= 2;
+        return winValue;
         break;
     case (playerChoice==="Paper" || computerChoice==="Rock"):
         alert("You win");
+        winValue= 2;
+        return winValue;
         break;
     case (playerChoice==="Paper" || computerChoice==="Paper"):
         alert("it's a draw");
+        winValue= 0;
+        return winValue;
         break;
     case (playerChoice==="Paper" || computerChoice==="Scissors"):
         alert("The computer wins");
+        winValue= 1;
+        return winValue;
         break;
     case (playerChoice==="Scissors" || computerChoice==="Rock"):
         alert("The computer wins");
+        winValue= 1;
+        return winValue;
         break;
     case (playerChoice==="Scissors" || computerChoice==="Paper"):
         alert("You win");
+        winValue= 2;
+        return winValue;
         break;
     case (playerChoice==="Scissors" || computerChoice==="Scissors"):
         alert("it's a draw");
+        winValue= 0;
+        return winValue;
         break;
   }
+
 
 }
 
@@ -76,7 +98,16 @@ function playerChoice(choice){
 }
 
 
+// it manages who wins (so the first to get 5 points)
+function game() {
 
+}
+
+
+//restarts the game (resets all the counters)
+function resClick() {
+
+}
 
 
 
